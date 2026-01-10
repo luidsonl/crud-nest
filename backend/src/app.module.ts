6438554cdb9d43a279f0d347b36286e71daf3833
Luidson Lima <luidsonlsti@gmail.com>
2026-01-10 00:42:52 -0300

    feat: create user and bookmark modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AuthModule, BookmarkModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
