1514296f1fb69f325e130eefd0777d9274a831e5
Luidson Lima <luidsonlsti@gmail.com>
2026-01-10 02:36:25 -0300

    feat: configure Prisma with PostgreSQL adapter and update environment settings

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import 'dotenv/config';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient} from '../../generated/prisma/client'
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient implements OnModuleInit{
+    constructor() {
+        const adapter = new PrismaPg({connectionString: process.env.DATABASE_URL!});
+        super({adapter});
+    }
+
+    async onModuleInit() {
+        await this.$connect();
+    }
+}
