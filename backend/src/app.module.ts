fcb6686e6edde16b637cbc99bafa38d4539dc630
Luidson Lima <luidsonlsti@gmail.com>
2026-01-24 14:08:31 -0300

    feat: start bookmark module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,6 +6,7 @@ import { AuthModule } from './auth/auth.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
   imports: [
